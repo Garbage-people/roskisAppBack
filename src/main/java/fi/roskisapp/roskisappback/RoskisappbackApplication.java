6c4c67e693b31097daf1dcb824fd0871d29d7923
Aleksi Hyppönen <aleksi.hypponen@gmail.com>
2024-02-06 10:38:26 +0200

    project initialization

---
+++ src/main/java/fi/roskisapp/roskisappback/RoskisappbackApplication.java
@@ -0,0 +1,13 @@
+package fi.roskisapp.roskisappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoskisappbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoskisappbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/roskisapp/roskisappback/RoskisappbackApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.roskisapp.roskisappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoskisappbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
