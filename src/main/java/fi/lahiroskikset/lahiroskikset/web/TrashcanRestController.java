b4b8f123a9f42b50639f8976beed71f7f4176cc2
Aleksi Hyppönen <aleksi.hypponen@gmail.com>
2024-04-09 11:55:12 +0300

    sorting imports

--- src/main/java/fi/lahiroskikset/lahiroskikset/web/TrashcanRestController.java
+++ src/main/java/fi/lahiroskikset/lahiroskikset/web/TrashcanRestController.java
@@ -13,15 +13,7 @@ import fi.lahiroskikset.lahiroskikset.domain.Trashcan;
 import fi.lahiroskikset.lahiroskikset.domain.TrashcanRepository;
 import fi.lahiroskikset.lahiroskikset.service.DistanceCalculator;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RestController
 @RequestMapping("/api/trashcans")
